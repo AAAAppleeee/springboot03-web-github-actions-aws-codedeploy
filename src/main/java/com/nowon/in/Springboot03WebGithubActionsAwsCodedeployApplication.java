f6a39c0b8008153ddcfa82a87533f8025d61c97f
Administrator <Administrator@304-13>
2023-12-26 15:07:49 +0900

    1111

---
+++ src/main/java/com/nowon/in/Springboot03WebGithubActionsAwsCodedeployApplication.java
@@ -0,0 +1,13 @@
+package com.nowon.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot03WebGithubActionsAwsCodedeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot03WebGithubActionsAwsCodedeployApplication.class, args);
+	}
+
+}
